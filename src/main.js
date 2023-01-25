f70eca799b58f2a60225efe0e4f605471897e1b1
Divine <ss6169@srmist.edu.in>
2023-01-25 08:52:00 +0530

    FINAL

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { initializeApp } from 'firebase/app'
 
-createApp(App).mount('#app')
+/* code from our Firebase console */
+const firebaseConfig = {
+  apiKey: "AIzaSyBzJWnBgF7A7KrwXQKFip33CdxK3Mok-eI",
+  authDomain: "auth-vue-91844.firebaseapp.com",
+  projectId: "auth-vue-91844",
+  storageBucket: "auth-vue-91844.appspot.com",
+  messagingSenderId: "233381265680",
+  appId: "1:233381265680:web:530f2ebba64468747b6595",
+  measurementId: "G-ZKS5WCEW1D"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
